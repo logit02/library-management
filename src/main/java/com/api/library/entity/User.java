1b7f5f680e925f85fae77e14a1857e363e6ee712
Inesa Toroyan <inesatoroyan@Inesas-MacBook-Pro.local>
2022-05-10 13:26:51 +0400

    first commit

---
+++ src/main/java/com/api/library/entity/User.java
@@ -0,0 +1,12 @@
+package com.api.library.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
